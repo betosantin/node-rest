59c9a418ff0e889c66d39741d180272cde1035c6
Roberto Santin <betosantin@gmail.com>
2021-03-30 19:10:26 -0300

    Adicionado middleware com autenticacao via jwt token

--- index.js
+++ index.js
@@ -2,11 +2,44 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const JWTSecret = "asifjsdhfwejthwelkgjdvnxcmvndgsl2sdv325";
 
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+function auth(req, res, next){
+
+    const authToken = req.headers['authorization'];
+
+    if(authToken != undefined){
+        const bearer = authToken.split(' ');
+
+        var token = bearer[1];
+
+        jwt.verify(token, JWTSecret, (err, data) =>{
+            if(err){
+                res.status(401);
+                res.json({err: "Token inválido"});
+            } else {
+                req.token = token;
+                req.loggedUser = {id: data.id, email: data.email};
+                next();
+            }
+        });
+
+
+    } else {
+        res.status(401);
+        res.json({err: "Token inválido"});
+    }
+
+   
+}
+
 
 var db = {
     games: [
@@ -28,15 +61,29 @@ var db = {
             year: 2021,
             price: 103.27,
         },
+    ],
+    users: [
+        {
+            id: 1,
+            name: "Roberto Santin",
+            email: "betosantin@gmail.com",
+            password: "123"
+        },
+        {
+            id: 2,
+            name: "Roberto Santin 2",
+            email: "betosantin@hotmail.com",
+            password: "123"
+        }
     ]
 }
 
-app.get("/games", (req, res) => {
+app.get("/games", auth, (req, res) => {
     res.statusCode = 200;
     res.json(db.games);
 });
 
-app.get("/game/:id", (req, res) => {
+app.get("/game/:id", auth, (req, res) => {
     var id = req.params.id;
 
     if (isNaN(id)) {
@@ -55,7 +102,7 @@ app.get("/game/:id", (req, res) => {
     }
 });
 
-app.post("/game", (req, res) => {
+app.post("/game", auth, (req, res) => {
 
     var { title, price, year } = req.body;
 
@@ -73,7 +120,7 @@ app.post("/game", (req, res) => {
     res.sendStatus(200);
 });
 
-app.delete("/game/:id", (req, res) => {
+app.delete("/game/:id", auth, (req, res) => {
 
     var id = req.params.id;
 
@@ -94,7 +141,7 @@ app.delete("/game/:id", (req, res) => {
     }
 });
 
-app.put("/game/:id", (req, res) => {
+app.put("/game/:id", auth, (req, res) => {
 
     var id = req.params.id;
 
@@ -127,6 +174,44 @@ app.put("/game/:id", (req, res) => {
             res.sendStatus(404)
         }
     }
+});
+
+app.post("/auth", (req, res) =>{
+
+    var { email, password } = req.body;
+
+    if (email != undefined && password != undefined) {
+
+        var user = db.users.find(u => u.email == email);
+
+        if(user != undefined) {
+
+            if(user.password == password){
+
+                jwt.sign({id: user.id, email: user.email}, JWTSecret, {expiresIn: '1h'}, (err, token)=>{
+                    if(err){
+                        res.status(400);
+                        res.json({err: "Falha interna"});
+                    } else{
+                        res.status(200);
+                        res.json({token: token});
+                    }
+                });
+            } else {
+                res.status(401);
+                res.json({token: "Senha inválida"});
+            }
+
+        } else {
+            res.status(404);
+            res.json({ err: "Usuário não encontrado" });
+        }
+
+    } else {
+        res.status(400);
+        res.json({ err: "E-mail ou Senha inválido" });
+    }
+
 });
 
 app.listen(4001, () => {
